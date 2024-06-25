c2509c5c376eca0243eeccb3197c48f8cc5340ac
NaaAbbey <dnyabbey@st.ug.edu.gh>
2024-06-25 10:00:04 +0000

    home screen completed

--- PaymentApp/components/home.js
+++ PaymentApp/components/home.js
@@ -1,15 +1,126 @@
-import { View, StyleSheet, Image, Text } from "react-native";
+import { View, StyleSheet, Image, Text, ScrollView } from "react-native";
+import { Option } from "./options";
+import { Transaction } from "./transaction";
 
 export function HomeScreen () {
     return(
-        <View style={styles.container}>
-
-            
-        </View>
+        <ScrollView style={styles.container} showsHorizontalScrollIndicator={false}>
+            <View style={styles.header}>
+                <Image source={require('../assets/profile.png')} style={styles.profile}/>
+                <View style={styles.textblock}>
+                    <Text style={styles.welcome}>Welcome Back</Text>
+                    <Text style={styles.name}>Eric Atsu</Text>
+                </View>
+               <View style={styles.search}>
+               <Image source={require('../assets/search.png')} style={styles.searchicon}/>
+                </View> 
+            </View>
+            <Image source={require('../assets/Card.png')} style={styles.card}/>
+            <View style={styles.options}>
+                <Option icon={require('../assets/send.png')} name={'Send'}/>
+                <Option icon={require('../assets/recieve.png')} name={'Receive'}/>
+                <Option icon={require('../assets/loan.png')} name={'Loan'}/>
+                <Option icon={require('../assets/topUp.png')} name={'TopUp'}/>
+            </View>
+            <View style={styles.titleView}>
+                <Text style={styles.title}>Transaction</Text>
+                <Text style={styles.more}>See All</Text>
+            </View>
+            <View>
+                <Transaction
+                 icon={require('../assets/apple.png')} 
+                 name={'Apple Store'}
+                 type={'Entertainment'}
+                 cost={'-$5,99'}
+                  />
+                  <Transaction
+                 icon={require('../assets/spotify.png')} 
+                 name={'Spotify'}
+                 type={'Music'}
+                 cost={'-$12,99'}
+                  />
+                  <Transaction
+                 icon={require('../assets/moneyTransfer.png')} 
+                 name={'Money Transfer'}
+                 type={'Transaction'}
+                 cost={'$300'}
+                  />
+                  <Transaction
+                 icon={require('../assets/grocery.png')} 
+                 name={'Grocery'}
+                 type={'Grocery'}
+                 cost={'-$88'}
+                  />
+            </View>
+        </ScrollView>
     )}
 
 
 const styles = StyleSheet.create({
+    container:{
+        padding: '20px',
+    },
+    header:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: '20px'
+    },
+    profile: {
+        width: '60px',
+        height: '60px',
+    },
+    textblock: {
+        flex: 1.5,
+        marginLeft: '50px',
+    },
+    welcome:{
+        fontWeight: 'semibold',
+        color: 'gray',
+        fontSize: '15px',
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: '30px',
+    },
+    search: {
+        width: '60px',
+        height: '60px',
+        backgroundColor: 'lightgray',
+        borderRadius: '50%', 
+        justifyContent: 'center',
+        alignItems: 'center',
 
-
+    },
+    searchicon: {
+        width: '30px',
+        height: '30px',
+    },
+    card: {
+        width: '100%',
+        height: '250px',
+        borderRadius: '25px',
+        marginTop: '30px',
+        marginBottom: '30px',
+    },
+    options: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: '35px',
+    },
+    titleView:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: '20px',
+        alignContent: 'center',
+    },
+    title:{
+        fontSize: '24px',
+        fontWeight: 'bold',
+    },
+    more: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: '18px',
+        alignSelf: 'center',
+    },
 })
---
+++ PaymentApp/components/transaction.js
@@ -0,0 +1,67 @@
+import { View, StyleSheet, Text, Image } from "react-native";
+
+export function Transaction ({icon, name, type, cost}) {
+    return(
+        <View style={styles.transactionContainer}>
+            <View style={styles.icon}>
+                <Image source={icon} style={styles.iconImg}/>
+            </View>
+            <View style={styles.details}>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.type}>{type}</Text>
+            </View>
+            <View>
+                <Text style={styles.cost}>{cost}</Text>
+            </View>
+        </View>
+    )}
+
+
+const styles = StyleSheet.create({
+    transactionContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignContent: 'center',
+        height: '75px',
+        alignItems: 'center',
+        marginBottom: '15px',
+    },
+    icon: {
+        backgroundColor: 'lightgray',
+        width: '65px',
+        height: '65px',
+        borderRadius: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+    },
+    iconImg: {
+        width: '28px',
+        height: '30px',
+    },
+    details: {
+        flex: 1.5,
+        marginLeft: '30px',
+        height: '100%',
+        alignItems: 'baseline',
+        
+    },
+    name: {
+        fontSize: '22px',
+        fontWeight: 'bold',
+        flex: 1,
+    },
+    type: {
+        fontSize: '19px',
+        fontWeight: 'semibold',
+        color: 'grey',
+        flex: 1,
+    },
+    cost: {
+        fontSize: '24px',
+        fontWeight: 'bold',
+        height: '100%',
+
+    }
+
+})
